262d8b354992e002b6297c5ab7b189a013c7fceb
rooooose <shir.flower@gmail.com>
2023-05-04 13:37:15 +0200

    test

--- Google Crawling/crawling.py
+++ Google Crawling/crawling.py
@@ -34,7 +34,7 @@ def scrape_google_and_order(query, year, company, found_list, doubt_list):
 
     # Find all google search results (g class) and sub-results (d4rhi class)
     allData = soup.find_all("div",{"class":["g", "d4rhi"]})
-
+    print(allData)
     if len(allData) > 0:
         print("oui")
 
