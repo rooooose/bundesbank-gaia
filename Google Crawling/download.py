edb9296d64088e5f04c1445eac4254a9a9e2b591
rooooose <shir.flower@gmail.com>
2023-05-19 17:27:00 +0200

    upload directly to dropbox

--- Google Crawling/download.py
+++ Google Crawling/download.py
@@ -2,6 +2,18 @@ import requests
 from vars_for_requests import headers
 import os
 from write_results import write_json
+import dropbox
+
+def dropbox_upload(filepath, company):
+    dbx = dropbox.Dropbox('sl.BeofrB0-YwYsFkzC9bl0A1TnVqYeuStHmMG0OtwmoFGKG7U9AzLAgptpVo5osRTt_SJ934z0-ZJSuMEcc0c4HGpE7cxj2FevP9Kvbruyy63jTgtpJ6RYVyGihsoTE6_lMkbG0eR7A78')
+    
+    with open(filepath, "rb") as f:
+        # print("cc")
+        # filename = filepath.rsplit("/", 1)[1]
+        # print(filename)
+        dbx.files_upload(f.read(), "/2023-05-19 (msci)/"+company+"/"+filepath, mode=dropbox.files.WriteMode("overwrite"))
+    
+    os.remove(filepath)
 
 def download_pdf(link, yearString, companyName):
 
@@ -14,14 +26,13 @@ def download_pdf(link, yearString, companyName):
 
     if status_code == 200:
 
-        if not os.path.exists("resultPDFs/"+companyName):
-            os.makedirs("resultPDFs/"+companyName)
-
-        pdf = open(os.path.join("resultPDFs/"+companyName, yearString+"_report.pdf"), 'wb')
+        pdf = open(yearString + "_report.pdf", 'wb')
         pdf.write(response.content)
         pdf.close()
     
-        filepath = "resultPDFs/" + companyName + "/" + yearString + "_report.pdf"
+        filepath = yearString + "_report.pdf"
+
+        dropbox_upload(filepath, companyName)
         return filepath
     else:
         print("GET REQUEST FAILED")
