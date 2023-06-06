a5c08f6008b0936b679f179a5f03dd1b4646d76c
rooooose <shir.flower@gmail.com>
2023-06-06 09:36:42 +0200

    make_company_list file

--- Google Crawling/main.py
+++ Google Crawling/main.py
@@ -1,13 +1,12 @@
-import csv
 import json
 from crawling import scrape_google_and_order
 from download import download_pdf
 from text_reading import read_and_reorder_pdf
 from write_results import write_stats
-import pandas as pd
-import re, os, time
+import os, time
 import shutil
 import dropbox
+from make_company_list import make_clean_list
 
 def find_where_to_start(file):
 
@@ -28,134 +27,40 @@ def find_where_to_start(file):
 
 
 years_to_search = ["2017", "2018", "2019", "2020", "2021", "2022"]
-# doubt_count = 0
 
-msci_list = pd.read_csv("Google Crawling/msci.csv")["Name"]
-dax_list = pd.read_csv("Google Crawling/dax.csv")["COMPANIES"]
-# print(dax_list)
-
-not_allowed_terms_old = [
-    ' inc',
-    ' group',
-    " class",
-    " ltd",
-    " plc",
-    " ag",
-    " sa",
-    " corp",
-    " entertainment",
-    " holding",
-    " holdings",
-    " company",
-    " reit",
-    " pharmaceuticals",
-    " pharmaceutical",
-    " nv", 
-    " industries",
-    " communications",
-    " corporation",
-    " resources",
-    " vacations",
-    " solutions",
-    " a",
-    " stapled units",
-    " n",
-    " units",
-    " unit",
-    " investment",
-    " b",
-    " energia",
-    " energias"
-]
-
-not_allowed_terms = [
-    " class",
-    " nv", 
-    " a",
-    " stapled units",
-    " n",
-    " units",
-    " unit",
-    " b",
-    " i"
-]
-
-tolerated_terms = [
-    ' inc',
-    ' group',
-    " ltd",
-    " plc",
-    " ag",
-    " sa",
-    " corp",
-    " entertainment",
-    " holding",
-    " holdings",
-    " company",
-    " reit",
-    " pharmaceuticals",
-    " pharmaceutical",
-    " industries",
-    " communications",
-    " corporation",
-    " resources",
-    " vacations",
-    " solutions",
-    " investment",
-    " energia",
-    " energias"
-]
-
-
-msci_list = msci_list.apply(str.lower)
-
-for key, company in msci_list.items():
-    for word in not_allowed_terms:
-        found = re.search(word + "(\s|$)", company)
-        if found:
-            company = company.split(word, 1)[0]
-            msci_list.iloc[key] = company
-    
-    for word in tolerated_terms:
-        found = re.search(word + "(\s|$)", company)
-        if found:
-            company = company.split(word, 1)[0] + word
-            msci_list.iloc[key] = company
-
-companies = pd.concat([dax_list, msci_list])
-print(len(companies))
+companies = make_clean_list()
 
 dbx = dropbox.Dropbox('sl.BfETvybupeFZL7XDB3riszW1D7fXDg1Sa4N6W8JYkLEWENJp7rWzSNtnQhAxEWZrU2jUPpKiGP5Z_XRtsV0fbxGHIsGJ68TVu-EA7qNs9J0CTHgq1e7q-gxkaMDHrz_FGTOdwZPZ-Pc:EUR')
 
+# PART 1 : FIND LINKS
+last_year_index, last_comp_index = find_where_to_start('stopped_search_at.txt')
+year_changed = False
 
-# last_year_index, last_comp_index = find_where_to_start('stopped_search_at.txt')
+for year in years_to_search[last_year_index:]: 
 
-# year_changed = False
-
-# for year in years_to_search[last_year_index:]: 
-
-#     # when we change year, we want to start at the first comany (-1 because we want to start 0 and we do +1)
-#     if year_changed:
-#         last_comp_index = -1
-#     if last_comp_index+1 == len(companies):
-#         break
-#     for company in companies[last_comp_index+1:]:
-#         print(company)
-#         scrape_google_and_order(company + " sustainability report " + year + " filetype:pdf", year, company)
-#         f = open('stopped_search_at.txt','w')
-#         f.write(year + "-" + company)
-#         time.sleep(0.8)
+    # when we change year, we want to start at the first comany (-1 because we want to start 0 and we do +1)
+    if year_changed:
+        last_comp_index = -1
+    if last_comp_index+1 == len(companies):
+        break
+    for company in companies[last_comp_index+1:]:
+        print(company)
+        scrape_google_and_order(company + " sustainability report " + year + " filetype:pdf", year, company)
+        f = open('stopped_search_at.txt','w')
+        f.write(year + "-" + company)
+        time.sleep(0.8)
     
-#     year_changed = True
+    year_changed = True
 
-#     write_stats(year, "0")
+    write_stats(year, "0")
 
 
+#TO COMMENT WHEN DOWNLOAD STARTED
+original_found = 'found_results_0.json'
+new_found = 'found_results_1.json'
+shutil.copyfile(original_found, new_found)
 
-# original_found = 'found_results_0.json'
-# new_found = 'found_results_1.json'
-# shutil.copyfile(original_found, new_found)
-
+# PART 2 : DOWNLOAD AND READ PDFS
 with open('doubt_results_0.json','r') as file:
     try:
         doubt_list = json.load(file)
@@ -169,7 +74,6 @@ with open('found_results_0.json','r') as file:
         found_list = {}
 
 last_year_index, last_comp_index = find_where_to_start('stopped_download_at.txt')
-
 year_changed = False
 
 for year in years_to_search[last_year_index:]:
---
+++ Google Crawling/make_company_list.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import re
+
+
+msci_list = pd.read_csv("Google Crawling/msci.csv")["Name"]
+dax_list = pd.read_csv("Google Crawling/dax.csv")["COMPANIES"]
+
+not_allowed_terms_old = [
+    ' inc',
+    ' group',
+    " class",
+    " ltd",
+    " plc",
+    " ag",
+    " sa",
+    " corp",
+    " entertainment",
+    " holding",
+    " holdings",
+    " company",
+    " reit",
+    " pharmaceuticals",
+    " pharmaceutical",
+    " nv", 
+    " industries",
+    " communications",
+    " corporation",
+    " resources",
+    " vacations",
+    " solutions",
+    " a",
+    " stapled units",
+    " n",
+    " units",
+    " unit",
+    " investment",
+    " b",
+    " energia",
+    " energias"
+]
+
+not_allowed_terms = [
+    " class",
+    " nv", 
+    " a",
+    " stapled units",
+    " n",
+    " units",
+    " unit",
+    " b",
+    " i"
+]
+
+tolerated_terms = [
+    ' inc',
+    ' group',
+    " ltd",
+    " plc",
+    " ag",
+    " sa",
+    " corp",
+    " entertainment",
+    " holding",
+    " holdings",
+    " company",
+    " reit",
+    " pharmaceuticals",
+    " pharmaceutical",
+    " industries",
+    " communications",
+    " corporation",
+    " resources",
+    " vacations",
+    " solutions",
+    " investment",
+    " energia",
+    " energias"
+]
+
+def make_clean_list():
+
+    msci_list = msci_list.apply(str.lower)
+
+    for key, company in msci_list.items():
+        for word in not_allowed_terms:
+            found = re.search(word + "(\s|$)", company)
+            if found:
+                company = company.split(word, 1)[0]
+                msci_list.iloc[key] = company
+        
+        for word in tolerated_terms:
+            found = re.search(word + "(\s|$)", company)
+            if found:
+                company = company.split(word, 1)[0] + word
+                msci_list.iloc[key] = company
+
+    companies = pd.concat([dax_list, msci_list])
+    return companies
