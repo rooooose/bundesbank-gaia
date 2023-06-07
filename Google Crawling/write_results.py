0e233abc59a3db8a96524e4dc2c02387d8dd5ff7
rooooose <shir.flower@gmail.com>
2023-06-07 15:15:37 +0200

    few form corrections

--- Google Crawling/write_results.py
+++ Google Crawling/write_results.py
@@ -1,7 +1,7 @@
 import json
 
 def write_stats(year, i):
-  """For given year, onverts json files in json dicts, counts how many pdfs were found and doubtful, how many pdfs couldn't be downloaded, and writes everything in the right stats file
+  """For given year, converts json files in json dicts, counts how many pdfs were found and doubtful, how many pdfs couldn't be downloaded, and writes everything in the right stats file
 
   Args:
       year (string): year for which the statistics have to be calculated
@@ -13,7 +13,6 @@ def write_stats(year, i):
       file_data = json.load(file)
     except:
       file_data = {}
-      # doubt_count_total = 0
 
     if year in file_data.keys():
       doubt_count_year = len(file_data[year])
@@ -22,19 +21,18 @@ def write_stats(year, i):
     doubt_count_total = sum(len(list) for list in file_data.values())
 
   with open('found_results_' + i + '.json','r') as file:
-        try:
-          file_data = json.load(file)
-          found_count_year = len(file_data[year])
-        except:
-          file_data = {}
-          found_count_year = 0
-        
-        if year in file_data.keys():
-          found_count_year = len(file_data[year])
-        else:
-          found_count_year = 0
-
-        found_count_total = sum(len(list) for list in file_data.values())
+    try:
+      file_data = json.load(file)
+      found_count_year = len(file_data[year])
+    except:
+      file_data = {}
+      found_count_year = 0
+    
+    if year in file_data.keys():
+      found_count_year = len(file_data[year])
+    else:
+      found_count_year = 0
+    found_count_total = sum(len(list) for list in file_data.values())
   
   to_find_per_year = doubt_count_year + found_count_year
   to_find_total = doubt_count_total + found_count_total
@@ -43,6 +41,12 @@ def write_stats(year, i):
   conclusion_doubt = str(doubt_count_total) + " doubtful results until now"
   conclusion_found_total = str(found_count_total) + " found results until now"
   tofind_total = str(to_find_total) + " results to find"
+
+  f = open('stats' + i + '.txt','a+')
+  f.write(conclusion_found)
+  f.write(conclusion_doubt+'\n')
+  f.write(conclusion_found_total+'\n')
+  f.write(tofind_total+'\n')
   
   if i == "1" :
     with open('exception_at_download.json','r') as file:
@@ -59,14 +63,7 @@ def write_stats(year, i):
     exception_per_year = exception_count_year*100/to_find_per_year
     percentage_except = str(exception_per_year) + "% couldn't be downloaded\n"
     count_except = str(exception_count_total) + " exceptions until now"
-  
-  f = open('stats' + i + '.txt','a+')
-  f.write(conclusion_found)
-  f.write(conclusion_doubt+'\n')
-  f.write(conclusion_found_total+'\n')
-  f.write(tofind_total+'\n')
 
-  if i == "1" :
     f.write(percentage_except)
     f.write(count_except +'\n\n')
   
